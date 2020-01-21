8b562f8dbdeccde018e245274792438f3c295acf
Victor Lebrun <victor.lebrun011@gmail.com>
2020-01-21 13:58:29 +0100

    Restore to blank template

--- src/app.js
+++ src/app.js
@@ -1,29 +1,20 @@
 import React from 'react'
-import { createApp, registerFocusManger, registerServiceWorker } from 'renative'
-import { navStructure } from './nav'
-import ScreenHome from './screenHome'
-import ScreenMyPage from './screenMyPage'
-import ScreenModal from './screenModal'
-import Menu from './menu'
-
-import '../platformAssets/runtime/fontManager'
-
-registerFocusManger({ focused: 'opacity: 0.4' })
-registerServiceWorker()
-
-// Flag to enable yellow warnings
-console.disableYellowBox = true
+import { View, Text } from 'react-native'
+import { Api } from 'renative'
+import WatchFace from './Components/WatchFace/WatchFace'
 
 let AppContainer
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    AppContainer = createApp(navStructure, { ScreenHome, ScreenMyPage, ScreenModal, Menu })
-  }
-
   render() {
-    return AppContainer
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Text>{'This is blank app'}</Text>
+        <Text>{`platform: ${Api.platform}`}</Text>
+        <Text>{`factor: ${Api.formFactor}`}</Text>
+        <WatchFace />
+      </View>
+    )
   }
 }
 
